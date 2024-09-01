dc42a6bec3145ef9902599b95b01fa5b41f81188
mr-chelyshkin <argent-keel-0z@icloud.com>
2024-09-01 16:47:57 +0300

    draft

--- terraform/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  lambda_functions = toset([for f in fileset("${path.root}/../cmd", "*") : f if fileexists("${path.root}/../cmd/${f}/main.go")])
-  region           = "eu-central-1"
-}
--- terraform/modules/lambda/localstack_backend.hcl
+++
@@ -1,3 +0,0 @@
-backend "local" {
-  path = "terraform.tfstate"
-}
