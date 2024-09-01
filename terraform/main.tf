fde9a12807b7c70158fc1a5d12588e0fb6f1753d
mr-chelyshkin <argent-keel-0z@icloud.com>
2024-09-01 15:42:02 +0300

    draft

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,47 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = var.region
+
+  dynamic "endpoints" {
+    for_each = var.use_localstack ? [1] : []
+    content {
+      iam    = var.localstack_endpoint
+      lambda = var.localstack_endpoint
+      s3     = var.localstack_endpoint
+      sts    = var.localstack_endpoint
+    }
+  }
+
+  s3_use_path_style           = var.use_localstack
+  skip_credentials_validation = var.use_localstack
+  skip_metadata_api_check     = var.use_localstack
+  skip_requesting_account_id  = var.use_localstack
+
+  access_key = var.use_localstack ? "test" : null
+  secret_key = var.use_localstack ? "test" : null
+}
+
+
+variable "use_localstack" {
+  description = "Whether to use LocalStack instead of real AWS"
+  type        = bool
+  default     = false
+}
+
+variable "localstack_endpoint" {
+  description = "LocalStack endpoint"
+  type        = string
+  default     = "http://localhost:4566"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
 module "lambda_functions" {
   for_each = local.lambda_functions
   source   = "./modules/lambda"
