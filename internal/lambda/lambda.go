3ca935a1cd11556042e139be6f17ea2b4ac19318
mr-chelyshkin <argent-keel-0z@icloud.com>
2024-09-08 10:36:53 +0300

    draft

--- internal/lambda/lambda.go
+++ internal/lambda/lambda.go
@@ -41,27 +41,35 @@ func NewLambda(handlers map[string]HandleFunc) (*lambda, error) {
 }
 
 // Handle processes the incoming Lambda event
-func (l *lambda) Handle(ctx context.Context, event json.RawMessage) (events.APIGatewayProxyResponse, error) {
+func (l *lambda) Handle(ctx context.Context, event json.RawMessage) (resp events.APIGatewayProxyResponse, err error) {
 	l.logger.Debug("Received event", zap.String("event", string(event)))
 
-	var base BaseRequest
-	if err := json.Unmarshal(event, &base); err != nil {
-		l.logger.Error("Invalid request format", zap.Error(err))
-		return NewResponse(400, http.StatusText(400), nil).ToAPIGatewayProxyResponse()
-	}
+	var (
+		base   BaseRequest
+		logMsg string
+	)
+	defer func() {
+		if err != nil {
+			l.logger.Error(logMsg, zap.Error(err), zap.Int("statusCode", resp.StatusCode), zap.String("action", base.Action))
+		} else {
+			l.logger.Info(logMsg, zap.Int("statusCode", resp.StatusCode), zap.String("action", base.Action))
+		}
+	}()
 
+	if err = json.Unmarshal(event, &base); err != nil {
+		logMsg = "Invalid request format"
+		return NewResponse(http.StatusBadRequest, http.StatusText(http.StatusBadRequest), nil).ToAPIGatewayProxyResponse()
+	}
 	handler, ok := l.handlers[base.Action]
 	if !ok {
-		l.logger.Warn("Unknown action", zap.String("action", base.Action))
-		return NewResponse(404, http.StatusText(404), nil).ToAPIGatewayProxyResponse()
+		logMsg = "Unknown action"
+		return NewResponse(http.StatusNotFound, http.StatusText(http.StatusNotFound), nil).ToAPIGatewayProxyResponse()
 	}
-
 	result, err := handler(ctx, base.Data)
 	if err != nil {
-		l.logger.Error("Error processing request", zap.Error(err))
-		return NewResponse(500, http.StatusText(500), nil).ToAPIGatewayProxyResponse()
+		logMsg = "Error processing request"
+		return NewResponse(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError), nil).ToAPIGatewayProxyResponse()
 	}
-
-	l.logger.Debug("Request processed successfully", zap.Any("result", result))
-	return NewResponse(200, http.StatusText(200), result).ToAPIGatewayProxyResponse()
+	logMsg = "Request processed successfully"
+	return NewResponse(http.StatusOK, http.StatusText(http.StatusOK), result).ToAPIGatewayProxyResponse()
 }
