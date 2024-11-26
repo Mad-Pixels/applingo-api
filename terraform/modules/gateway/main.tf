0d23566b292cf0cc320ea7323eb8451b64ae526f
mr-chelyshkin <argent-keel-0z@icloud.com>
2024-11-26 12:56:09 +0200

    rest

--- terraform/modules/gateway/main.tf
+++ terraform/modules/gateway/main.tf
@@ -3,12 +3,16 @@ data "aws_region" "current" {}
 
 locals {
   manifest = templatefile("../../../openapi-interface/.tmpl/openapi.yaml", {
-    project             = var.project
-    name                = var.api_name
-    use_localstack      = var.use_localstack
-    invoke_lambdas_arns = var.invoke_lambdas_arns
-    region              = data.aws_region.current.name
-    account_id          = try(data.aws_caller_identity.current.account_id, "*")
+    project        = var.project
+    name           = var.api_name
+    use_localstack = var.use_localstack
+    region         = data.aws_region.current.name
+    account_id     = try(data.aws_caller_identity.current.account_id, "*")
+
+    api_dictionaries = var.invoke_lambdas_arns["api-dictionaries"].arn
+    api_categories   = var.invoke_lambdas_arns["api-categories"].arn
+    api_reports      = var.invoke_lambdas_arns["api-reports"].arn
+    api_urls         = var.invoke_lambdas_arns["api-urls"].arn
   })
 }
 
