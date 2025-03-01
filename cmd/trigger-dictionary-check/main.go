5b96cd0c2ce7787d8e15d6fbfcecf3931cebbe1d
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-03-01 08:37:50 +0200

    prepare handler

--- cmd/trigger-dictionary-check/main.go
+++ cmd/trigger-dictionary-check/main.go
@@ -5,10 +5,15 @@ import (
 	"encoding/json"
 	"os"
 	"runtime/debug"
+	"time"
 
 	"github.com/Mad-Pixels/applingo-api/dynamodb-interface/gen/applingoprocessing"
+	"github.com/Mad-Pixels/applingo-api/pkg/chatgpt"
+	"github.com/Mad-Pixels/applingo-api/pkg/cloud"
+	"github.com/Mad-Pixels/applingo-api/pkg/httpclient"
 	"github.com/Mad-Pixels/applingo-api/pkg/serializer"
 	"github.com/Mad-Pixels/applingo-api/pkg/trigger"
+	"github.com/Mad-Pixels/applingo-api/pkg/utils"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -17,17 +22,45 @@ import (
 	"github.com/rs/zerolog"
 )
 
+const (
+	defaultBackoff    = 15 * time.Second
+	defaultRetries    = 2
+	defaultMaxWorkers = 5
+)
+
 var (
-	awsRegion = os.Getenv("AWS_REGION")
+	serviceProcessingBucket = os.Getenv("SERVICE_PROCESSING_BUCKET")
+	serviceForgeBucket      = os.Getenv("SERVICE_FORGE_BUCKET")
+	lambdaTimeout           = os.Getenv("LAMBDA_TIMEOUT_SECONDS")
+	awsRegion               = os.Getenv("AWS_REGION")
+	openaiToken             = os.Getenv("OPENAI_KEY")
+
+	gptClient *chatgpt.Client
+	dbDynamo  *cloud.Dynamo
+	s3Bucket  *cloud.Bucket
+
+	timeout = utils.GetTimeout(lambdaTimeout, 240*time.Second)
 )
 
 func init() {
 	debug.SetGCPercent(500)
 
-	_, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(awsRegion))
+	gptClient = chatgpt.MustClient(
+		httpclient.New().
+			WithTimeout(timeout).
+			WithMaxRetries(defaultRetries, defaultBackoff).
+			WithRetryCondition(func(statusCode int, responseBody string) bool {
+				return statusCode >= 500 && statusCode < 600
+			}),
+		openaiToken,
+	)
+
+	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(awsRegion))
 	if err != nil {
 		panic("unable to load AWS SDK config: " + err.Error())
 	}
+	s3Bucket = cloud.NewBucket(cfg)
+	dbDynamo = cloud.NewDynamo(cfg)
 }
 
 func handler(ctx context.Context, log zerolog.Logger, record json.RawMessage) error {
@@ -40,7 +73,12 @@ func handler(ctx context.Context, log zerolog.Logger, record json.RawMessage) er
 		return errors.Wrap(err, "failed to extract item from DynamoDB event")
 	}
 
-	log.Error().Any("item", item).Msg("item")
+	switch dynamoDBEvent.EventName {
+	case "INSERT":
+		log.Error().Any("item", item).Msg("INSERT")
+	case "MODIFY":
+		log.Error().Any("item", item).Msg("MODIFY")
+	}
 	return nil
 }
 
