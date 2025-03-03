9e2f1c93e4aed8a475073214c667ce2f8638eb02
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-03-03 11:59:57 +0200

    comment some languages

--- lingo-interface/types/language_code.go
+++ lingo-interface/types/language_code.go
@@ -11,24 +11,24 @@ type LanguageCode int
 const (
 	LangEN LanguageCode = iota // English
 	LangES                     // Spanish
-	LangFR                     // French
-	LangDE                     // German
-	LangIT                     // Italian
-	LangPT                     // Portuguese
-	LangRU                     // Russian
-	LangNL                     // Dutch
-	LangPL                     // Polish
-	LangCS                     // Czech
-	LangSV                     // Swedish
-	LangDA                     // Danish
-	LangFI                     // Finnish
-	LangNO                     // Norwegian
-	LangHI                     // Hindi
-	LangBN                     // Bengali
-	LangID                     // Indonesian
-	LangAR                     // Arabic
-	LangHE                     // Hebrew
-	LangFA                     // Persian
+	// LangFR                     // French
+	LangDE // German
+	LangIT // Italian
+	LangPT // Portuguese
+	LangRU // Russian
+	// LangNL                     // Dutch
+	// LangPL                     // Polish
+	// LangCS                     // Czech
+	// LangSV                     // Swedish
+	// LangDA                     // Danish
+	// LangFI                     // Finnish
+	// LangNO                     // Norwegian
+	// LangHI                     // Hindi
+	// LangBN                     // Bengali
+	// LangID                     // Indonesian
+	// LangAR                     // Arabic
+	LangHE // Hebrew
+	// LangFA                     // Persian
 )
 
 // String returns the ISO 639-1 code
@@ -38,8 +38,8 @@ func (l LanguageCode) String() string {
 		return "en"
 	case LangES:
 		return "es"
-	case LangFR:
-		return "fr"
+	// case LangFR:
+	// 	return "fr"
 	case LangDE:
 		return "de"
 	case LangIT:
@@ -48,32 +48,32 @@ func (l LanguageCode) String() string {
 		return "pt"
 	case LangRU:
 		return "ru"
-	case LangNL:
-		return "nl"
-	case LangPL:
-		return "pl"
-	case LangCS:
-		return "cs"
-	case LangSV:
-		return "sv"
-	case LangDA:
-		return "da"
-	case LangFI:
-		return "fi"
-	case LangNO:
-		return "no"
-	case LangHI:
-		return "hi"
-	case LangBN:
-		return "bn"
-	case LangID:
-		return "id"
-	case LangAR:
-		return "ar"
+	// case LangNL:
+	// 	return "nl"
+	// case LangPL:
+	// 	return "pl"
+	// case LangCS:
+	// 	return "cs"
+	// case LangSV:
+	// 	return "sv"
+	// case LangDA:
+	// 	return "da"
+	// case LangFI:
+	// 	return "fi"
+	// case LangNO:
+	// 	return "no"
+	// case LangHI:
+	// 	return "hi"
+	// case LangBN:
+	// 	return "bn"
+	// case LangID:
+	// 	return "id"
+	// case LangAR:
+	// 	return "ar"
 	case LangHE:
 		return "he"
-	case LangFA:
-		return "fa"
+	// case LangFA:
+	// 	return "fa"
 	default:
 		return "unknown"
 	}
@@ -86,8 +86,8 @@ func (l LanguageCode) Name() string {
 		return "English"
 	case LangES:
 		return "Spanish"
-	case LangFR:
-		return "French"
+	// case LangFR:
+	// 	return "French"
 	case LangDE:
 		return "German"
 	case LangIT:
@@ -96,32 +96,32 @@ func (l LanguageCode) Name() string {
 		return "Portuguese"
 	case LangRU:
 		return "Russian"
-	case LangNL:
-		return "Dutch"
-	case LangPL:
-		return "Polish"
-	case LangCS:
-		return "Czech"
-	case LangSV:
-		return "Swedish"
-	case LangDA:
-		return "Danish"
-	case LangFI:
-		return "Finnish"
-	case LangNO:
-		return "Norwegian"
-	case LangHI:
-		return "Hindi"
-	case LangBN:
-		return "Bengali"
-	case LangID:
-		return "Indonesian"
-	case LangAR:
-		return "Arabic"
+	// case LangNL:
+	// 	return "Dutch"
+	// case LangPL:
+	// 	return "Polish"
+	// case LangCS:
+	// 	return "Czech"
+	// case LangSV:
+	// 	return "Swedish"
+	// case LangDA:
+	// 	return "Danish"
+	// case LangFI:
+	// 	return "Finnish"
+	// case LangNO:
+	// 	return "Norwegian"
+	// case LangHI:
+	// 	return "Hindi"
+	// case LangBN:
+	// 	return "Bengali"
+	// case LangID:
+	// 	return "Indonesian"
+	// case LangAR:
+	// 	return "Arabic"
 	case LangHE:
 		return "Hebrew"
-	case LangFA:
-		return "Persian"
+	// case LangFA:
+	// 	return "Persian"
 	default:
 		return "Unknown"
 	}
@@ -130,9 +130,9 @@ func (l LanguageCode) Name() string {
 // AllLanguageCodes returns a slice of all available language codes
 func AllLanguageCodes() []LanguageCode {
 	return []LanguageCode{
-		LangEN, LangES, LangFR, LangDE, LangIT, LangPT, LangRU,
-		LangNL, LangPL, LangCS, LangSV, LangDA, LangFI, LangNO,
-		LangHI, LangBN, LangID, LangAR, LangHE, LangFA,
+		LangEN, LangES, LangDE, LangIT, LangPT, LangRU, LangHE,
+		// LangFR, LangNL, LangPL, LangCS, LangSV, LangDA, LangFI,
+		// LangHI, LangBN, LangID, LangAR, LangFA, LangNO,
 	}
 }
 
@@ -153,8 +153,8 @@ func ParseLanguageCode(s string) (LanguageCode, error) {
 		return LangEN, nil
 	case "es":
 		return LangES, nil
-	case "fr":
-		return LangFR, nil
+	// case "fr":
+	// 	return LangFR, nil
 	case "de":
 		return LangDE, nil
 	case "it":
@@ -163,32 +163,32 @@ func ParseLanguageCode(s string) (LanguageCode, error) {
 		return LangPT, nil
 	case "ru":
 		return LangRU, nil
-	case "nl":
-		return LangNL, nil
-	case "pl":
-		return LangPL, nil
-	case "cs":
-		return LangCS, nil
-	case "sv":
-		return LangSV, nil
-	case "da":
-		return LangDA, nil
-	case "fi":
-		return LangFI, nil
-	case "no":
-		return LangNO, nil
-	case "hi":
-		return LangHI, nil
-	case "bn":
-		return LangBN, nil
-	case "id":
-		return LangID, nil
-	case "ar":
-		return LangAR, nil
+	// case "nl":
+	// 	return LangNL, nil
+	// case "pl":
+	// 	return LangPL, nil
+	// case "cs":
+	// 	return LangCS, nil
+	// case "sv":
+	// 	return LangSV, nil
+	// case "da":
+	// 	return LangDA, nil
+	// case "fi":
+	// 	return LangFI, nil
+	// case "no":
+	// 	return LangNO, nil
+	// case "hi":
+	// 	return LangHI, nil
+	// case "bn":
+	// 	return LangBN, nil
+	// case "id":
+	// 	return LangID, nil
+	// case "ar":
+	// 	return LangAR, nil
 	case "he":
 		return LangHE, nil
-	case "fa":
-		return LangFA, nil
+	// case "fa":
+	// 	return LangFA, nil
 	default:
 		return 0, errors.New("invalid language code")
 	}
