e008848c2a4cd0f1d6965e33c3ef07434f4774b5
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-03-13 07:17:59 +0200

    draft

--- cmd/trigger-processing-check/modify.go
+++ cmd/trigger-processing-check/modify.go
@@ -3,12 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
-	"reflect"
 	"time"
 
 	"github.com/Mad-Pixels/applingo-api/dynamodb-interface/gen/applingodictionary"
 	"github.com/Mad-Pixels/applingo-api/dynamodb-interface/gen/applingoprocessing"
-	"github.com/Mad-Pixels/applingo-api/dynamodb-interface/persistent"
 	"github.com/Mad-Pixels/applingo-api/pkg/utils"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -16,24 +14,12 @@ import (
 )
 
 type changes struct {
-	// check
-	description bool
-	overview    bool
-	topic       bool
-	level       bool
-	languages   bool
-	words       bool
-	subcategory bool
-	promptCraft bool
-	promptCheck bool
-
 	// process
 	score  bool
 	upload bool
 
 	// action flags.
 	needProcess bool
-	needUpdate  bool
 
 	// data
 	oldItem *applingoprocessing.SchemaItem
@@ -44,16 +30,6 @@ func detectChanges(old, new *applingoprocessing.SchemaItem) changes {
 	c := changes{
 		score:  old.Score != new.Score,
 		upload: old.Upload != new.Upload,
-
-		description: old.Description != new.Description,
-		overview:    old.Overview != new.Overview,
-		topic:       old.Topic != new.Topic,
-		level:       old.Level != new.Level,
-		languages:   old.Languages != new.Languages,
-		words:       old.Words != new.Words,
-		subcategory: old.Subcategory != new.Subcategory,
-		promptCraft: old.PromptCraft != new.PromptCraft,
-		promptCheck: old.PromptCheck != new.PromptCheck,
 
 		oldItem: old,
 		newItem: new,
@@ -82,66 +58,13 @@ func modify(ctx context.Context, e events.DynamoDBEventRecord) error {
 		return fmt.Errorf("failed extract old data: %w", err)
 	}
 	c := detectChanges(old, new)
-	check(&c)
 
 	if c.needProcess {
 		if err := processRecord(ctx, &c); err != nil {
 			return fmt.Errorf("failed to process record: %w", err)
 		}
 	}
-	if c.needUpdate {
-		if err := updateRecord(ctx, *c.newItem); err != nil {
-			return fmt.Errorf("failed to update record with validated data: %w", err)
-		}
-	}
 	return nil
-}
-
-func check(c *changes) {
-	beforeChecks := *c.newItem
-
-	if c.description {
-		c.newItem.Description = persistent.FieldDescriptionOrDefault(c.newItem.Description, c.oldItem.Description)
-	}
-	if c.topic {
-		c.newItem.Topic = persistent.FieldTopicOrDefault(c.newItem.Topic, c.oldItem.Topic)
-	}
-	if c.level {
-		c.newItem.Level = persistent.FieldLevelOrDefault(c.newItem.Level, c.oldItem.Level)
-	}
-	if c.languages {
-		c.newItem.Languages = persistent.FieldLanguagesOrDefault(c.newItem.Languages, c.oldItem.Languages)
-	}
-	if c.words {
-		c.newItem.Words = persistent.FieldWordsOrDefault(c.newItem.Words, c.oldItem.Words)
-	}
-	if c.subcategory {
-		c.newItem.Subcategory = persistent.FieldSubcategoryOrDefault(c.newItem.Subcategory, c.oldItem.Subcategory)
-	}
-	if c.promptCraft {
-		c.newItem.PromptCraft = persistent.FieldPromtOrDefault(c.newItem.PromptCraft, c.oldItem.PromptCraft)
-	}
-	if c.promptCheck {
-		c.newItem.PromptCheck = persistent.FieldPromtOrDefault(c.newItem.PromptCheck, c.oldItem.PromptCheck)
-	}
-	c.needUpdate = !reflect.DeepEqual(beforeChecks, *c.newItem)
-}
-
-func updateRecord(ctx context.Context, item applingoprocessing.SchemaItem) error {
-	condition := expression.And(
-		expression.AttributeExists(expression.Name(applingoprocessing.ColumnId)),
-		expression.AttributeExists(expression.Name(applingoprocessing.ColumnCreated)),
-	)
-	dynamoItem, err := applingoprocessing.PutItem(item)
-	if err != nil {
-		return err
-	}
-	return dbDynamo.Put(
-		ctx,
-		applingoprocessing.TableSchema.TableName,
-		dynamoItem,
-		condition,
-	)
 }
 
 func processRecord(ctx context.Context, c *changes) error {
