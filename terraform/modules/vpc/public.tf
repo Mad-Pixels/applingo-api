133a99e4ca5d55f449e267f70c213e32565ba79b
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-04-29 20:41:41 +0300

    [vpc] add custom acl

--- terraform/modules/vpc/public.tf
+++ terraform/modules/vpc/public.tf
@@ -49,3 +49,77 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public[0].id
 }
+
+
+resource "aws_network_acl" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(
+    var.shared_tags,
+    {
+      "Type" = "public-acl",
+      "Name" = "${var.name}-public-acl",
+    }
+  )
+}
+
+resource "aws_network_acl_rule" "public_inbound_allow_all" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "public_inbound_allow_ipv6_all" {
+  network_acl_id  = aws_network_acl.public.id
+  rule_number     = 101
+  egress          = false
+  protocol        = "-1"
+  rule_action     = "allow"
+  ipv6_cidr_block = "::/0"
+}
+
+resource "aws_network_acl_rule" "public_outbound_allow_all" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "public_outbound_allow_ipv6_all" {
+  network_acl_id  = aws_network_acl.public.id
+  rule_number     = 101
+  egress          = true
+  protocol        = "-1"
+  rule_action     = "allow"
+  ipv6_cidr_block = "::/0"
+}
+
+resource "aws_network_acl_rule" "public_inbound_allow_metadata" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 110
+  egress         = false
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "169.254.0.0/16"
+}
+
+resource "aws_network_acl_rule" "public_outbound_allow_metadata" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 110
+  egress         = true
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "169.254.0.0/16"
+}
+
+resource "aws_network_acl_association" "public" {
+  count = var.use_public_subnets ? length(aws_subnet.public) : 0
+
+  subnet_id       = aws_subnet.public[count.index].id
+  network_acl_id  = aws_network_acl.public.id
+}
